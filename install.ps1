2046b1a2afbdecc9f40c36e561dd0759b7e68784
John Camp <jjcamp@gmail.com>
2016-12-13 22:29:37 -0600

    Adds encoding check to installer

--- install.ps1
+++ install.ps1
@@ -1,5 +1,21 @@
+$encoding = 'utf8'
 if (!(Test-Path $PROFILE)) {
     New-Item $PROFILE -Force -ItemType File -ErrorAction Stop
+}
+else {
+    $sr = [System.IO.StreamReader]::new($PROFILE, [System.Text.Encoding]::ASCII, $true)
+    $sr.Peek() > $null
+    $enc = $sr.CurrentEncoding
+    $sr.Close() > $null
+    switch -Regex ($enc.HeaderName) {
+        'utf-16BE' { $encoding = "bigendianunicode"; break }
+        'utf-(\d.*)' {
+            if ($matches[1] -eq 16) { $encoding = "unicode"; break }
+            $encoding = "utf$($matches[1])"
+            break
+        }
+        'ascii' { $encoding = "ascii"; break }
+    }
 }
 $modulesDirFound = $env:PsModulePath -match '^(.*?);' 
 if (!($modulesDirFound)) {
@@ -11,20 +27,18 @@ if (!(Test-Path $modulesDir)) {
     New-Item $modulesDir -Force -ItemType Directory -ErrorAction Stop
 }
 $thisPath = Split-Path $MyInvocation.MyCommand.Path -Parent
-Copy-Item -Path $thisPath -Destination $modulesDir -Recurse
+Copy-Item -Path $thisPath -Destination $modulesDir -Recurse -Force
 
 $profileContents = Get-Content $PROFILE
 $profileApp = ''
 
 if (!(Select-String -InputObject $profileContents -Pattern 'Set-PsPrompt' -Quiet -SimpleMatch)) {
-    $profileApp = 'Import-Module -Name PsPrompt'
     $profileApp += [System.Environment]::NewLine
-    $profileApp += 'Set-PsPrompt' 
-    $profileChanged = $true
+    $profileApp += 'Set-PsPrompt'
 }
 
 if ($profileApp -ne '') {
-    Out-File -InputObject $profileApp -FilePath $PROFILE -Append
+    Out-File -InputObject $profileApp -FilePath $PROFILE -Append -Encoding $encoding
 }
 
 Write-Host "`nPsPrompt has been installed.  Restart your console (recommended) or run &(`$profile)."
